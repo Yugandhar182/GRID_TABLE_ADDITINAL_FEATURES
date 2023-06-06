5d9c28ec2f52a3b201ddd9faeb632e399e7de091
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-06 16:45:35 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,115 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { onMount } from 'svelte';
+	import { Grid } from 'gridjs';
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	let grid;
+	let jsonData = [];
+  
+	onMount(async () => {
+	  const response = await fetch("https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154");
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+	  console.log(jsonData, "jsonData");
+	  grid = new Grid({
+		columns: ['firstName', 'surname', 'email', 'mobile', 'Actions'],
+		data: jsonData.map((item) => [
+		  item.firstName,
+		  item.surname,
+		  item.email,
+		  item.mobile,
+		  
+		  createActionsCell(item),
+		]),
+		className: {
+		  table: 'gridjs-table',
+		  th: 'gridjs-th',
+		  td: 'gridjs-td',
+		},
+		search: true, // Enable search
+		sort: true, // Enable sorting
+		pagination: {
+		  enabled: true, // Enable pagination
+		  limit: 10, // Set the number of rows per page
+		},
+	  }).render(document.getElementById('grid-container'));
+	});
+  
+	function createActionsCell(item) {
+	  const editButton = document.createElement('button');
+	  editButton.className = 'btn btn-primary';
+	  editButton.innerHTML = 'Edit';
+	  editButton.addEventListener('click', () => {
+		handleEdit(item);
+	  });
+  
+	  const deleteButton = document.createElement('button');
+	  deleteButton.className = 'btn btn-danger';
+	  deleteButton.innerHTML = 'Delete';
+	  deleteButton.addEventListener('click', () => {
+		confirmDelete(item);
+	  });
+  
+	  const cell = document.createElement('div');
+	  cell.className = 'btn-group';
+	  cell.appendChild(editButton);
+	  cell.appendChild(deleteButton);
+  
+	  return cell;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+  
+	async function handleEdit(item) {
+	  // Example: Show edit popup
+	  alert(`Editing ${item.firstName} ${item.surname}`);
+	  // You can replace the alert with your own custom edit logic or open a popup/modal for editing
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+  
+	function confirmDelete(item) {
+	  const confirmed = confirm('Are you sure you want to delete this item?');
+	  if (confirmed) {
+		handleDelete(item);
+	  }
+	}
+  
+	async function handleDelete(item) {
+	  // Handle delete logic here
+	  console.log('Deleting', item);
+  
+	  const apiUrl = `https://api.recruitly.io/api/candidate/${item.id}?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`;
+	  const response = await fetch(apiUrl, {
+		method: 'DELETE',
+		headers: {
+		  'Content-Type': 'application/json'
 		}
+	  });
+  
+	  if (response.ok) {
+		console.log('Item deleted successfully');
+		// Remove the deleted item from the grid
+		jsonData = jsonData.filter((candidate) => candidate.id !== item.id);
+		grid.update({
+		  data: jsonData.map((item) => [
+			item.firstName,
+			item.surname,
+			item.email,
+			item.mobile,
+			createActionsCell(item),
+		  ])
+		});
+	  } else {
+		console.error('Failed to delete item');
+	  }
 	}
-</style>
+  </script>
+  
+  <div id="grid-container"></div>
+  
+  <style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+	.btn-group {
+	  display: flex;
+	  gap: 5px;
+	}
+  </style>
+  
+  
+  
